dba2e77225c2348f6862a03a0afbe68b1a249dcc
FiveTech Software <antonio.fivetech@gmail.com>
2021-12-08 09:24:23 +0100

    Update go64.bat

--- go64.bat
+++ go64.bat
@@ -1,4 +1,6 @@
 wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+copy c:\opencv\build\x64\vc15\bin\opencv_world454.dll .
+copy c:\opencv\build\x64\vc15\bin\opencv_videoio_msmf454_64.dll .
 @set oldpath=%Path%
 @set oldinclude=%INCLUDE%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
